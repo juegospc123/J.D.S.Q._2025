43668e8957d3914b1ce9756d8e07479af6efc7ee
Juegos PC <150499818+juegospc123@users.noreply.github.com>
2025-03-18 17:50:35 -0300

    Entrada de Datos

---
+++ JavaApplication62/src/javaapplication62/EjeN_3.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication62;
+ import java.util.Scanner;
+
+public class EjeN_3 {
+    public static void main(String[] args) {
+        
+     Scanner dato = new Scanner(System.in);
+     
+        long edad;
+        
+        System.out.println("ingrese un numero :");
+         edad= dato.nextLong();
+    
+        System.out.println("El numero ingresado es:"+edad);
+        
+    
+    
+    }
+    
+}
---
+++ JavaApplication62/src/javaapplication62/EjeN_5.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication62;
+
+/**
+ *
+ * @author LABO B CEN
+ */
+public class EjeN_5 {
+    
+}
